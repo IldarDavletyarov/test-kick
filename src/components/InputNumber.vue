2ea20b93412489634d4532e57bc4ecd65650f2d0
IldarDavletyarov <ildardaf@gmail.com>
2021-06-25 20:18:16 +0300

    Add [valid isNumber, limitFractionDigits] InputNumber

--- src/components/InputNumber.vue
+++ src/components/InputNumber.vue
@@ -13,11 +13,30 @@
 import ChevronUp from '../icons/chevron-up.vue';
 import ChevronDown from '../icons/chevron-down.vue';
 
+const MAX_FRACTION_DIGITS = 8;
+
 const transformCommaToDot = (v) => v.replace(',', '.');
+
+const transformLimitFractionDigits = (v) => {
+  const separator = '.';
+  if (!v.includes(separator)) {
+    return v;
+  }
+  let split = v.split(separator);
+  let fraction = split[split.length - 1].slice(0, MAX_FRACTION_DIGITS);
+  return [split.slice(0, split.length - 1), fraction].join(separator);
+}
 
 const transforms = [
   transformCommaToDot,
+  transformLimitFractionDigits,
 ];
+
+const isNumber = (str) => {
+  if (typeof str != 'string') return false // we only process strings!  
+  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
 
 export default {
   components: {
@@ -45,9 +64,19 @@ export default {
   methods: {
     onInput(event) {
       let value = event.target.value || '';
+
+      if (!isNumber(value)) {
+        event.target.value = this.value; // previous valid number
+        return;
+      }
+      if (typeof value === 'number') {
+        console.log(value, this.value);
+      }
+
       transforms.forEach(t => {
         value = t(value);
       });
+      event.target.value = value;
       this.$emit('input', value);
     },
   },
