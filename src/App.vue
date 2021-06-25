9538b013613828754d6f33f75d3f8b00df3c622a
IldarDavletyarov <ildardaf@gmail.com>
2021-06-25 22:33:52 +0300

    Fix [reset button style] App componen

--- src/App.vue
+++ src/App.vue
@@ -17,4 +17,26 @@ export default {
 <style lang="stylus">
 #app
   font-family Arial, Helvetica, sans-serif
+button {
+  background: transparent;
+  box-shadow: 0px 0px 0px transparent;
+  border: 0px solid transparent;
+  text-shadow: 0px 0px 0px transparent;
+}
+
+button:hover {
+  background: transparent;
+  box-shadow: 0px 0px 0px transparent;
+  border: 0px solid transparent;
+  text-shadow: 0px 0px 0px transparent;
+}
+
+button:active {
+  outline: none;
+  border: none;
+}
+
+button:focus {
+  outline: 0;
+}
 </style>
